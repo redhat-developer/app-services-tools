d79bccf802569e2d1fa70ed2856507c041a19c60
Wojciech Trocki <wtrocki@redhat.com>
2022-02-03 12:10:35 +0000

    fix: errors in script

--- scripts/kafka.sh
+++ scripts/kafka.sh
@@ -10,17 +10,26 @@ then
 fi
 
 
-echo "creating kafka" 
+echo "Login and create kafka" 
 
 rhoas login --token=$OFFLINE_TOKEN
 
 rhoas kafka create --wait --name=$KAFKA_NAME
 
+sleep 10
+
+echo "Verify can connect to dataplane from RHOAS CLI" 
+
 rhoas kafka topic list
 
-rhoas service-account create --overwrite --file-format=env --output-file=sa.env --short-description=$SERVICE_ACCOUNT_NAME --
+echo "create service account for connectivity"
+
+rhoas service-account create --overwrite --file-format=env --output-file=sa.env --short-description=$SERVICE_ACCOUNT_NAME 
 
 source ./sa.env
+
+USER=$RHOAS_SERVICE_ACCOUNT_CLIENT_ID
+PASSWORD=$RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET
 
 if ! command -v kcat &> /dev/null
 then
@@ -28,3 +37,4 @@ then
     exit
 fi
 
+echo "TODO kcat produce consume"
