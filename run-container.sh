93727b13191f8bb8f02f867e424b365a42441732
Enda Phelan <ephelan@redhat.com>
2022-03-01 11:50:54 +0000

    update README

--- run-container.sh
+++ run-container.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker run -ti --rm --name rhoas-devsandbox --entrypoint /bin/bash quay.io/rhoas/tools
+registry=${REGISTRY:-quay.io}
+username=${USERNAME}
+
+docker run -ti --rm --name rhoas-devsandbox --entrypoint /bin/bash $registry/$username/tools
